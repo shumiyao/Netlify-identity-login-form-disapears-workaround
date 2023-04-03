1626570c3c17f14dde4083acf90c4b6ceb00c17f
shumiyao <50849841+shumiyao@users.noreply.github.com>
2023-04-03 16:58:13 +0100

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,18 @@
+module.exports = ({
+  pageExtensions: ["tsx"],
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules.push(
+      ...[
+        {
+          test: /\.ya?ml$/,
+          use: "js-yaml-loader",
+        },
+        {
+          test: /\.svg$/,
+          use: "@svgr/webpack",
+        },
+      ]
+    );
+    return config;
+  },
+});
